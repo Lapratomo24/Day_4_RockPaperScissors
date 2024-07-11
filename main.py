1e7e7bb4047dae6eed8ab9f474c90cafb80b0e4a
Aditya <lapratomo24@yahoo.com>
2024-07-11 23:55:09 +0200

    add game logic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Rock, Paper, Scissors
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+import random
+
+choices = [rock, paper, scissors]
+
+player_choice = int(input("Choose between 0 for rock, 1 for paper, or 2 for scissors --> "))
+
+if player_choice < 0 or player_choice >= 3:
+    print("Invalid number. Try again!")
+else:
+    print("You chose: ")
+    print(choices[player_choice])
+    
+    print("Computer chose")
+    computer_choice = random.randint(0, 2)
+    print(choices[computer_choice])
+    
+    if player_choice == computer_choice:
+        print("It's a draw.")
+    elif (player_choice == 0 and computer_choice == 2) or (player_choice == 1 and computer_choice == 0) or (player_choice == 2 and computer_choice == 1):
+        print("You won!")
+    else:
+        print("You lost!")
+
